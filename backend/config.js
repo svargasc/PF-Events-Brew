d69d917b92e26d7330ada433f9d82a6f125f0be9
svargasc <svc.vargas03@gmail.com>
2023-11-11 16:01:45 -0500

    added docker config

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,5 @@
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/eventsBrews";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/eventsBrew";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
